fa020f2c9e75d890db7de292bab77517f379a9af
Murali981 <muraliju981@gmail.com>
2024-12-09 13:28:33 +0530

    Completed the Implementation of Search blogs page which has basic filtering, mobile-friendly and a good looking design

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,9 +1,10 @@
 const SearchHeader = () => {
   return (
     <div className="text-center mb-12">
-      <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent mb-4">
+      <h1 className="text-4xl h-[58px] md:text-5xl font-bold bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent mb-4 tracking-wide leading-loose">
         Discover Knowledge
       </h1>
+
       <p className="text-gray-600 text-lg">
         Explore our collection of insightful articles
       </p>
